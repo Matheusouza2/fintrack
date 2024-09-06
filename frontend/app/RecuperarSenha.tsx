9aa092e52c28314d7ad10dee369227ae28484bd7
Pedro <pedrovnsr@gmail.com>
2024-09-06 17:06:39 -0300

    Teste 2

---
+++ frontend/app/RecuperarSenha.tsx
@@ -0,0 +1,17 @@
+import { ScrollView } from "react-native-gesture-handler";
+import { SafeAreaView } from "react-native-safe-area-context";
+import {styles} from  "../assets/styles/RecuperarSenha";
+import React from "react";
+
+export default function RecuperarSenha(){
+    return (
+
+        <SafeAreaView>
+            <ScrollView>
+
+            </ScrollView>
+            
+        </SafeAreaView>
+
+    );
+}
