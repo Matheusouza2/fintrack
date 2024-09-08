9ebe3352be4e3d9772c4303f8818f9f1dafb872b
RB-Matheus <typematt4101@gmail.com>
2024-09-08 15:51:54 -0300

    criando o controlador de usuarios, as assinaturas dos metodos para o crud sobre ele e estabelecendo os respectivos endpoints

---
+++ backend/UserController.js
@@ -0,0 +1,42 @@
+import { Request, Response } from "express";
+
+
+
+// *************************************************
+// Obtém um usuário em particular com base no seu ID
+// *************************************************
+export function getUserById(request, response) {
+
+}
+
+
+// *************************************************
+// Lista todos os usuários.
+// *************************************************
+export function getAllUsers(request, response) {
+
+}
+
+
+// *************************************************
+// Cadastra um novo usuário.
+// *************************************************
+export function createUser(request, response) {
+
+}
+
+
+// *************************************************
+// Atualiza os dados de um determinado usuário.
+// *************************************************
+export function updateUser(request, response) {
+
+}
+
+
+// *************************************************
+// Exclui a conta de um usuário específico.
+// *************************************************
+export function deleteUser(request, response) {
+    
+}
--- backend/routes.js
+++ backend/routes.js
@@ -1,4 +1,50 @@
 import express from "express";
 const routes = express.Router();
 
+
+
+// *********************************************
+// Endpoint para consultar os dados de um usuá- 
+// rio em particular.
+// *********************************************
+routes.get("/users/:id", (req, res) => {
+    // res.send("(GET) Endpoint 'api/users/:id' OK");
+});
+
+
+// *********************************************
+// Endpoint para listar todos os usuários cadas- 
+// trados.
+// *********************************************
+routes.get("/users", (req, res) => {
+    // res.send("(GET) Endpoint 'api/users' OK");
+});
+
+
+// *********************************************
+// Endpoint para cadastrar usuários.
+// *********************************************
+routes.post("/users", (req, res) => {
+    // res.send("(POST) Endpoint 'api/users' OK");
+});
+
+
+// *********************************************
+// Endpoint para atualizar os dados de um deter-
+// minado usuário.
+// *********************************************
+routes.put("/users/:id", (req, res) => {
+    // res.send("(PUT) Endpoint 'api/users/:id' OK");
+});
+
+
+// *********************************************
+// Endpoint para excluir a conta de um usuário
+// específico.
+// *********************************************
+routes.delete("/users/:id", (req, res) => {
+    // res.send("(DELETE) Endpoint 'api/users/:id' OK");
+});
+
+
 export default routes;
