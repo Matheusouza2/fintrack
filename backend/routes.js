119e7432fa93a914619d002bb01bfdefa4e3c2a9
dev-emanuel-py <emanuelflavio1402@gmail.com>
2024-09-08 16:24:29 -0300

    comentados

--- backend/routes.js
+++ backend/routes.js
@@ -5,18 +5,29 @@ const routes = express.Router();
 routes.use(express.json());
 
 
+// Rotas para transferencia 
+
+// Listar Transferencias
 routes.get("/transferencias", async (req, res) => {
 
 });
+
+// Criar transferencias
 routes.post("/transferencias/create", async (req, res) => {
 
 });
+
+// Atualizar Transferencias
 routes.put("/transferencias/:id", async (req, res) => {
 
 });
+
+// Apagar Transferencias
 routes.delete("/transferencias/:id", async (req, res) => {
 
 });
+
+// Mostrar uma transferencia por id
 routes.get("/transferencias/:id", async (req, res) => {
 
 });
