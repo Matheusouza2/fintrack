c9df4727cb11835946dbaff4b5f324a8d3da4352
dev-emanuel-py <emanuelflavio1402@gmail.com>
2024-09-08 16:16:52 -0300

    assinatura controller e rotas transferencias

---
+++ backend/controllers/transferenciasController.js
@@ -0,0 +1,22 @@
+import transferencias from "../models/transferencias.js";
+
+
+export async function CriarTransferencia(data) {
+    
+}
+
+export async function AlterarTransferencia(id){
+
+}
+
+export async function DeletarTransferencia(id) {
+    
+}
+
+export async function ListarTransferencia() {
+    
+}
+
+export async function MostrarPorId(id) {
+    
+}
--- backend/routes.js
+++ backend/routes.js
@@ -1,4 +1,25 @@
 import express from "express";
+import * as TransferController from "./controllers/transferenciasController.js"
+
 const routes = express.Router();
+routes.use(express.json());
+
+
+routes.get("/transferencias", async (req, res) => {
+
+});
+routes.post("/transferencias/create", async (req, res) => {
+
+});
+routes.put("/transferencias/:id", async (req, res) => {
+
+});
+routes.delete("/transferencias/:id", async (req, res) => {
+
+});
+routes.get("/transferencias/:id", async (req, res) => {
+
+});
+
 
 export default routes;
