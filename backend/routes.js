69cff69125f77abd0ec3bce3475ab27c041a7f3d
RB-Matheus <typematt4101@gmail.com>
2024-09-09 12:04:18 -0300

    Padronizando os endpoints e o controlador de usuarios

--- backend/routes.js
+++ backend/routes.js
@@ -3,47 +3,47 @@ const routes = express.Router();
 
 
 
-// *********************************************
-// Endpoint para consultar os dados de um usuá- 
-// rio em particular.
-// *********************************************
-routes.get("/users/:id", (req, res) => {
-    // res.send("(GET) Endpoint 'api/users/:id' OK");
+/**********************************************
+ Endpoint para consultar os dados de um usuário 
+ em particular.
+ *********************************************/
+routes.get("/usuarios/:id", (req, res) => {
+    // res.send("(GET) Endpoint 'api/usuarios/:id' OK");
 });
 
 
-// *********************************************
-// Endpoint para listar todos os usuários cadas- 
-// trados.
-// *********************************************
-routes.get("/users", (req, res) => {
-    // res.send("(GET) Endpoint 'api/users' OK");
+/**********************************************
+ Endpoint para listar todos os usuários cadas- 
+ trados.
+ *********************************************/
+routes.get("/usuarios", (req, res) => {
+    // res.send("(GET) Endpoint 'api/usuarios' OK");
 });
 
 
-// *********************************************
-// Endpoint para cadastrar usuários.
-// *********************************************
-routes.post("/users", (req, res) => {
-    // res.send("(POST) Endpoint 'api/users' OK");
+/**********************************************
+ Endpoint para cadastrar usuários.
+ *********************************************/
+routes.post("/usuarios", (req, res) => {
+    // res.send("(POST) Endpoint 'api/usuarios' OK");
 });
 
 
-// *********************************************
-// Endpoint para atualizar os dados de um deter-
-// minado usuário.
-// *********************************************
-routes.put("/users/:id", (req, res) => {
-    // res.send("(PUT) Endpoint 'api/users/:id' OK");
+/**********************************************
+ Endpoint para atualizar os dados de um deter-
+ minado usuário.
+ *********************************************/
+routes.put("/usuarios/:id", (req, res) => {
+    // res.send("(PUT) Endpoint 'api/usuarios/:id' OK");
 });
 
 
-// *********************************************
-// Endpoint para excluir a conta de um usuário
-// específico.
-// *********************************************
-routes.delete("/users/:id", (req, res) => {
-    // res.send("(DELETE) Endpoint 'api/users/:id' OK");
+/**********************************************
+ Endpoint para excluir a conta de um usuário
+ específico.
+ *********************************************/
+routes.delete("/usuarios/:id", (req, res) => {
+    // res.send("(DELETE) Endpoint 'api/usuarios/:id' OK");
 });
 
 
