4a0c6e9eacfc46239e360b01f04474a4be7387bd
Mraphaelpy <mraphael.py@gmail.com>
2024-09-09 19:07:06 -0300

    adicionado controllers e rotas, apenas assinaturas

---
+++ backend/controllers/controler_transacoes.js
@@ -0,0 +1,14 @@
+//controller de transacoes do usuario cadastrar, alterar, listar, excluir e mostrar(com base nos id)
+function listarTransacoesPorUsuario(req, res) {}
+function mostrarTransacaoPorUsuario(req, res) {}
+function cadastrarTransacaoPorUsuario(req, res) {}
+function alterarTransacaoPorUsuario(req, res) {}
+function excluirTransacaoPorUsuario(req, res) {}
+
+module.exports = {
+    listarTransacoesPorUsuario,
+    mostrarTransacaoPorUsuario,
+    cadastrarTransacaoPorUsuario,
+    alterarTransacaoPorUsuario,
+    excluirTransacaoPorUsuario,
+    };
--- backend/routes.js
+++ backend/routes.js
@@ -1,4 +1,18 @@
 import express from "express";
 const routes = express.Router();
+import {listarTransacoesPorUsuario,
+    mostrarTransacaoPorUsuario,
+    cadastrarTransacaoPorUsuario,
+    alterarTransacaoPorUsuario,
+    excluirTransacaoPorUsuario} from "../backend/controllers/controler_transacoes.js";
+
+//rotas dos controlers e trasacoes dos usuarios
+routes.get("/transacoes", listarTransacoesPorUsuario);
+routes.get("/transacoes/:id", mostrarTransacaoPorUsuario);
+routes.post("/transacoes", cadastrarTransacaoPorUsuario);
+routes.put("/transacoes/:id", alterarTransacaoPorUsuario);
+routes.delete("/transacoes/:id", excluirTransacaoPorUsuario);
+
+
 
 export default routes;
