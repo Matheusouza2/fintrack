35ab174de98e073d3cdf7bcdba4e9e17329b1686
Mraphaelpy <mraphael.py@gmail.com>
2024-09-11 12:15:31 -0300

    criacao das rotas de transacoes

--- backend/routes.js
+++ backend/routes.js
@@ -1,8 +1,20 @@
 import express from "express";
 import * as TransferenciasController from "./controllers/transferenciasController.js"
-
+import * as transacoesController from "./controllers/transacoesController.js"
 const routes = express.Router();
 routes.use(express.json());
+
+/*
+    Rotas para transferencias
+ */
+//rotas dos controlers e trasacoes dos usuarios
+routes.get("/transacoes", transacoesController.listarTransacoesPorUsuario);
+routes.get("/transacoes/:id", transacoesController.mostrarTransacaoPorUsuario);
+routes.post("/transacoes", transacoesController.cadastrarTransacaoPorUsuario);
+routes.put("/transacoes/:id", transacoesController.alterarTransacaoPorUsuario);
+routes.delete("/transacoes/:id", transacoesController.excluirTransacaoPorUsuario);
+
+
 
 
 /*
