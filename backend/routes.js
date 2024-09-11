36163bf5c1267d212d5443cca1448b8d79a4ecb4
Mraphaelpy <mraphael.py@gmail.com>
2024-09-11 11:39:15 -0300

    fix conflit

--- backend/routes.js
+++ backend/routes.js
@@ -1,7 +1,9 @@
 import express from "express";
+import * as transacoesController from "./controllers/transacoesController.js";
+import * as TransferenciasController from "./controllers/transferenciasController.js"
+
 const routes = express.Router();
 routes.use(express.json());
-import * as transacoesController from "./controllers/transacoesController.js";
 
 //rotas dos controlers e trasacoes dos usuarios
 routes.get("/transacoes", transacoesController.listarTransacoesPorUsuario);
@@ -10,4 +12,45 @@ routes.post("/transacoes", transacoesController.cadastrarTransacaoPorUsuario);
 routes.put("/transacoes/:id", transacoesController.alterarTransacaoPorUsuario);
 routes.delete("/transacoes/:id", transacoesController.excluirTransacaoPorUsuario);
 
+/*
+ Rotas para transferencia
+*/ 
+
+/* 
+Endpoint para Listar Transferencias
+*/
+routes.get("/transferencias", async (req, res) => {
+
+});
+
+/* 
+Endpoint para Criar Transferencias
+*/
+routes.post("/transferencias/create", async (req, res) => {
+
+});
+
+/* 
+Endpoint para Atualizar Transferencias
+*/
+routes.put("/transferencias/:id", async (req, res) => {
+
+});
+
+/* 
+Endpoint para Apagar Transferencias
+*/
+routes.delete("/transferencias/:id", async (req, res) => {
+
+});
+
+/* 
+Endpoint para Mostrar uma transferencia por id
+*/
+routes.get("/transferencias/:id", async (req, res) => {
+
+});
+
+
 export default routes;
+
