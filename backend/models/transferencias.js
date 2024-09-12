0335a098a7e911828a4a5c30f7c68bd9239d0298
Eládio Leal <eladiolink1@gmail.com>
2024-09-12 11:23:14 -0300

    Criacao das funçoes do model

--- backend/models/transferencias.js
+++ backend/models/transferencias.js
@@ -2,4 +2,12 @@ import { PrismaClient } from '@prisma/client';
 
 const db = new PrismaClient();
 
-export const transferencias = (req, res) => {}
+export const CriarTransferencias = (dadoTranferencia) => {}
+
+export const ListarTransferencias = (userId) => {}
+
+export const AlterarTransferencias = (tranferenciaId, alteracoes) => {}
+
+export const ExcluirTransferencia = (tranferenciaId) => {}
+
+export const TransferenciasPorId = (tranferenciaId) => {}
