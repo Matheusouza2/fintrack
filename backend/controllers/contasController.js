dad9ee2ccfa4272c0fbeb71d5a8f23aa80219956
Matheus Souza <matheusouza1997@gmail.com>
2024-09-12 10:32:05 -0300

    correção na assinatura dos metodos

--- backend/controllers/contasController.js
+++ backend/controllers/contasController.js
@@ -1,23 +1,12 @@
 /* Essa classe serve para adicionar os controller de contas que deve conter os métodos de cadastrar, 
 alterar, listar, excluir e mostrar uma única conta com base no id      */
 
+export function cadastrarContas(req, res) {}
 
-export function cadastrar_contas(req, res){
-   
-}
+export function alterarContas(req, res) {}
 
-export function alterar_contas(req, res){
+export function listarContas(req, res) {}
 
-}
+export function excluirContas(req, res) {}
 
-export function listar_contas(req, res){
-
-}
-
-export function excluir_contas(req, res){
-
-}
-
-export function mostrar_contas(req, res){
-    
-}
+export function mostrarContas(req, res) {}
