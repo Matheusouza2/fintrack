0f76ec8093750bcb0445851a45230b6bd396d971
Elinaldo Lopes de Castro Paes <155487389+ElinaldoLopes@users.noreply.github.com>
2024-09-13 15:02:41 -0300

    tela de login
    tela de login com cpf, senha e botão de recuperar senha

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,9 +1,10 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
-  );
+
+    return (
+        <Stack>
+            <Stack.Screen name="index" />
+        </Stack>
+    );
 }
--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,15 +1,52 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { KeyboardAvoidingView, TextInput, TouchableOpacity, Text, Image, View} from "react-native";
+import { styles } from "../assets/styles/login";
+import { Redirect } from "expo-router";
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+export default function App() {
+    // texto padrão improtado como constante
+    const texto = styles.text;
+
+    return (
+        
+        <KeyboardAvoidingView style={styles.container}>
+            
+            {/* logo */}
+            <Image source={require('../assets/images/logo.png')}
+            style={styles.logo}/> 
+
+            {/* input do cpf */}
+            <Text style={{...texto, color: '#56aaff', alignSelf: 'flex-start', marginLeft: '10%'}}>CPF</Text>
+            <TextInput 
+                keyboardType="numeric"
+                textContentType="username"
+                autoComplete="username"
+                autoCorrect={false}
+                style={styles.input}
+            />
+
+            {/* input da senha */}
+            <Text style={{...texto, color: '#56aaff', alignSelf: 'flex-start', marginLeft: '10%'}}>Senha</Text>
+            <TextInput 
+                textContentType="password"
+                autoCapitalize="none"
+                autoComplete="password"
+                secureTextEntry={true}
+                autoCorrect={false}
+                style={styles.input}
+            />
+
+            {/* botão de login */}
+            <TouchableOpacity style={styles.button}> 
+                {/* onPress={submit} */}
+                <Text style={{...texto, color: '#fff'}}>Entrar</Text>
+            </TouchableOpacity>
+
+            {/* texto de esqueceu a senha */}
+            <TouchableOpacity style={styles.esqueceu}>
+                <Text style={{...texto, color: '#56aaff', textDecorationLine: "underline"}}>Esqueceu sua senha?</Text>
+            </TouchableOpacity>
+
+        </KeyboardAvoidingView>
+    );
 }
