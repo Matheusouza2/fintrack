c54812000549a68bcd76329ae455676917549083
Gabriel  Rodrigues <95626184+Gabrielgfr@users.noreply.github.com>
2024-09-20 08:57:43 -0300

    Card Visão Geral

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Edit testeapp/index.tsx to edit this screen.</Text>
     </View>
   );
 }
