c8e059a161f12de867695c16d242f8752dbdf8f0
Mraphaelpy <mraphael.py@gmail.com>
2024-09-20 15:13:33 -0300

    criado card de economia

--- frontend/app/splashScreen/index.tsx
+++ frontend/app/splashScreen/index.tsx
@@ -4,26 +4,26 @@ import { styles } from "../../assets/styles/splashScreen.style";
 export default function Index() {
   return (
     <View style={styles.container}>
-        <View style={styles.itens}>
-      <Image
-      source={require("../../assets/imagens/logo.png")}
-      />
-      <View style={styles.circuloPai}>
+      <View style={styles.itens}>
+        <Image
+          source={require("../../assets/images/logo.png")}
+        />
+        <View style={styles.circuloPai}>
 
-        <View style= {[styles.circuloAzulEscuro, styles.circulo]}>
+          <View style={[styles.circuloAzulEscuro, styles.circulo]}>
+
+          </View>
+          <View style={[styles.circuloAzulClaro, styles.circulo]}>
+
+          </View>
+          <View style={[styles.circuloLaranja, styles.circulo]}>
+
+          </View>
+
 
         </View>
-        <View style= {[styles.circuloAzulClaro, styles.circulo]}>
-
-        </View>
-        <View style= {[styles.circuloLaranja, styles.circulo]}>
-
-        </View>
-
 
       </View>
-
-        </View>
     </View>
   );
 }
