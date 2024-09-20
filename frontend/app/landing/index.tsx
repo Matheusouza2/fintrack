589b6855514c3102bd10bca1ed91734233c94d49
RB-Matheus <typematt4101@gmail.com>
2024-09-20 20:15:19 -0300

    Implementando a tela de landing e fazendo o link com as telas de login e cadastro

---
+++ frontend/app/landing/index.tsx
@@ -0,0 +1,36 @@
+import { Link } from "expo-router";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import { styles } from "../../assets/styles/landing";
+
+
+export default function Landing() {
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.balao}>
+                <Image source={require('../../assets/imagens/logo.png')} style={styles.logo} />
+                <Text style={styles.titulo}>
+                    <Text style={styles.tituloFin}>Fin</Text>
+                    <Text style={styles.tituloTrack}>track</Text>
+                </Text>
+            </View>
+
+
+            <Image source={require('../../assets/imagens/finance-app-amico.svg')} style={styles.storysetImagem} />
+
+
+            <View style={styles.containerBotao}>
+                <TouchableOpacity style={styles.botaoLogar}>
+                    <Link href="/login">
+                        <Text style={styles.textoBotao}>Login</Text>
+                    </Link>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.botaoCadastrar}>
+                    <Link href="/cadastroUsuario">
+                        <Text style={styles.textoBotao}>Cadastre-se</Text>
+                    </Link>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
