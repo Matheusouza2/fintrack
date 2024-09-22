1897a8bc702e5dcec1dc8d6bace6dfd97153fc54
RB-Matheus <typematt4101@gmail.com>
2024-09-22 13:44:02 -0300

    Criando o controller de categorias e endpoints

--- backend/routes.js
+++ backend/routes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import * as usuarioController from "./controllers/controladorUsuarios.js";
 import * as contasController from "./controllers/contasController.js";
+import * as categoriasController from "./controllers/categoriasController.js";
+
 
 const routes = express.Router();
 
-/* Em routes criei as rotas nas quais as funções irão pecorrer   */
 
+// Endpoints referentes ao controller de contas
 routes.post("/contas", contasController.cadastrarConta);
 
 routes.put("/contas", contasController.alterarContas);
@@ -15,5 +17,19 @@ routes.get("/contas", contasController.listarContas);
 routes.delete("/contas/:id", contasController.excluirContas);
 
 routes.get("/contas/:id", contasController.mostrarContas);
+
+
+
+// Endpoints referentes ao controller de categorias
+routes.post("/categorias", categoriasController.cadastrarCategoria);
+
+routes.put("/categorias/:id", categoriasController.atualizarCategoria);
+
+routes.delete("/categorias/:id", categoriasController.excluirCategoria);
+
+routes.get("/categorias", categoriasController.listarCategorias);
+
+routes.get("/categorias/:id", categoriasController.obterCategoria);
+
 
 export default routes;
