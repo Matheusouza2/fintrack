03f7d316feba1927a741af64ef6470de248bf98e
dev-emanuel-py <emanuelflavio1402@gmail.com>
2024-09-27 14:05:38 -0300

    criação do model e base de categoria

---
+++ backend/models/categorias.js
@@ -0,0 +1,13 @@
+import { PrismaClient } from '@prisma/client';
+
+const db = new PrismaClient();
+
+export const CriarCategoria = (data) => {}
+
+export const ListarCategorias = (Id) => {}
+
+export const AlterarCategoria = (Id, alteracoes) => {}
+
+export const ExcluirCategoria = (Id) => {}
+
+export const BuscarCategoria = (Id) => {}
