2a98bead7140dcfd5d29929f43a5081fb7f04fcc
catarinaUni <catarina.sampaio@discente.univasf.edu.br>
2024-09-28 20:57:42 -0300

    [backend] Criar o model e o banco de objetivos financeiros

---
+++ backend/models/objetivosFinanceiros.js
@@ -0,0 +1,23 @@
+import { PrismaClient } from "@prisma/client";
+
+const db = new PrismaClient();
+
+//novo objetivo financeiro
+export const criarObjetivoFinanceiro = async (dadosObjetivo) => {
+
+};
+
+// listar todos os objetivos financeiros
+export const listarObjetivosFinanceiros = async () => {
+
+};
+
+// atualizar um objetivo financeiro
+export const atualizarObjetivoFinanceiro = async (id, atualizacoes) => {
+
+};
+
+// deletar um objetivo financeiro
+export const deletarObjetivoFinanceiro = async (id) => {
+
+};
