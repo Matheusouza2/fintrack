93c032c53e93ff20ddd26835e054769ec2367b19
RB-Matheus <typematt4101@gmail.com>
2024-10-02 20:21:19 -0300

    Corrigindo o visual da tela de nova transferência

--- frontend/app/novaTransferencia/index.tsx
+++ frontend/app/novaTransferencia/index.tsx
@@ -1,10 +1,14 @@
+import { Image, View, Text, TextInput, Button, StyleSheet, TouchableOpacity } from 'react-native';
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { Link } from 'expo-router';
 import { Picker } from '@react-native-picker/picker';
+import { styles } from "../../assets/styles/novaTransferencia";
 
 
-//componente funcional novaTransacao e seta useState para criar a descrição, data, tipo de Transferência, conta de Origem e conta de Destino
-const novaTransacao = () => {
+
+const novaTransferencia = () => {
+
+  const [valor, setValor] = useState('0,00');
   const [descricao, setDescricao] = useState('');
   const [data, setData] = useState('');
   const [tipoTransferencia, setTipoTransferencia] = useState('Não recorrente');
@@ -12,30 +16,57 @@ const novaTransacao = () => {
   const [contaDestino, setContaDestino] = useState('');
 
 
-  //retorna a view e coloca o valor inicial como 0 e exibe o texto de nova transferência
+  const handleMudancaValor = (text: string) => {
+
+    if (text === '') {
+      setValor('0,00');
+      return;
+    }
+
+    const entradaTratada = text.replace(/\D/g, '');
+    const quantia = parseFloat(entradaTratada) / 100;
+    const valorFormatado = quantia.toFixed(2).replace('.', ',');
+
+    setValor(valorFormatado);
+  };
+
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Nova Transferência</Text>
-      <Text style={styles.value}>R$ 0,00</Text>
 
+      <View style={styles.containerCabecalho}>
+        <Link href="/listarTransferencias">
+          <Image
+            source={require('../../assets/icons/iconeVoltar.png')}
+            style={styles.setaRetroceder}
+          />
+          </Link>
+        <Text style={styles.titulo}>Nova Transferência</Text>
+      </View>
 
-      //os TextInptut tem dois campos que são controlados pelo estado correspondente e onChangeText atualiza o estado
       <TextInput
-        style={styles.input}
-        placeholder="Descrição"//placeholder é um texto temporário
+        style={styles.campoValorEntrada}
+        keyboardType="numeric"
+        value={`R$ ${valor}`}
+        onChangeText={handleMudancaValor}
+      />
+
+      <TextInput
+        style={styles.campoEntrada}
+        placeholder="Descrição"
+        placeholderTextColor="#A9A9A9"
         value={descricao}
         onChangeText={setDescricao}
       />
 
       <TextInput
-        style={styles.input}
-        placeholder="Data"  //placeholder é um texto temporário
+        style={styles.campoEntrada}
+        placeholder="Data"
+        placeholderTextColor="#A9A9A9"
         value={data}
         onChangeText={setData}
       />
 
-
-      //O picker vc pode selecionar o tipo de transferência; que o valor é controldo pelo estado tipoTransferencia
       <Picker
         selectedValue={tipoTransferencia}
         onValueChange={(itemValue, itemIndex) => setTipoTransferencia(itemValue)}
@@ -46,58 +77,31 @@ const novaTransacao = () => {
         <Picker.Item label="Fixo Mensal" value="Fixo Mensal" />
       </Picker>
 
-
-      //tem dois dois campos que são controlados pelos estados que tem abaixo
-    <TextInput
-        style={styles.input}
+      <TextInput
+        style={styles.campoEntrada}
         placeholder="Conta Origem"
+        placeholderTextColor="#A9A9A9"
         value={contaOrigem}
         onChangeText={setContaOrigem}
       />
 
       <TextInput
-        style={styles.input}
+        style={styles.campoEntrada}
         placeholder="Conta Destino"
+        placeholderTextColor="#A9A9A9"
         value={contaDestino}
         onChangeText={setContaDestino}
       />
 
-      <Button title="Salvar" onPress={() => {}} />
+      <View style={styles.containerBotao}>
+        <TouchableOpacity style={styles.botaoSalvar}>
+            <Text style={styles.textoBotao}>Salvar</Text>
+        </TouchableOpacity>
+      </View>
+
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-    backgroundColor: '#F5F5F5',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    marginBottom: 20,
-  },
-  value: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    color: '#007AFF',
-    marginBottom: 20,
-  },
-  input: {
-    height: 40,
-    borderColor: '#CCC',
-    borderWidth: 1,
-    marginBottom: 20,
-    paddingHorizontal: 10,
-    backgroundColor: '#FFF',
-  },
-  picker: {
-    height: 50,
-    marginBottom: 20,
-  },
-});
 
-export default novaTransacao;
+export default novaTransferencia;
