76576eb11c6f4555c2b92b105d9e143727b7f42d
João Paulo <jhonbrandaoks83@gmail.com>
2024-10-24 17:35:22 -0300

    Add Regra de negocio Criar Categoria
    Add Regra de negocio Criar Categoria em controller e models

--- backend/controllers/categoriasController.js
+++ backend/controllers/categoriasController.js
@@ -17,9 +17,23 @@ export async function listarCategorias(req, res) {
 /***************************************************
  Cria uma categoria no sistema.
  **************************************************/
-export async function cadastrarCategoria(req, res) {
-
-}
+ export async function cadastrarCategoria(req, res) {
+    try {
+      const { nome, descricao, tipo, cor, icone } = req.body;
+  
+      
+      if (!nome || !tipo || !cor || !icone) {
+        return res.status(400).json({ message: 'Nome, tipo, cor e ícone são obrigatórios.' });
+      }
+      const novaCategoria = await CriarCategoria({ nome, descricao, tipo, cor, icone });
+  
+      return res.status(201).json(novaCategoria);
+    } catch (error) {
+      console.error('Erro ao criar categoria:', error);
+      return res.status(500).json({ message: 'Erro ao criar categoria.' });
+    }
+  }
+  
 
 
 /***************************************************
