b0d0c2be12ebb79eefb8b91e6cfbb339e8f301e0
Gabriel  Rodrigues <95626184+Gabrielgfr@users.noreply.github.com>
2024-10-31 17:54:25 -0300

    Regra de negócio de listar categorias

--- backend/models/categorias.js
+++ backend/models/categorias.js
@@ -4,7 +4,17 @@ const db = new PrismaClient();
 
 export const CriarCategoria = (data) => {}
 
-export const ListarCategorias = () => {}
+export const ListarCategorias = async () => {
+  try {
+    // Busca todas as categorias do banco de dados
+    const categorias = await db.categoria.findMany();
+    return categorias;
+  } catch (error) {
+    console.error("Erro ao listar categorias:", error);
+    throw error;  // Retorna o erro para o controlador lidar com ele
+  }
+};
+
 
 export const AlterarCategoria = (Id, alteracoes) => {}
 
