43ce444f3749b3bb0174acd0a5c595bd53f52276
dev-emanuel-py <emanuelflavio1402@gmail.com>
2024-11-02 17:47:17 -0300

    listar objetivos financeiros

--- backend/models/objetivosFinanceiros.js
+++ backend/models/objetivosFinanceiros.js
@@ -9,8 +9,13 @@ export const criarObjetivoFinanceiro = async (dadosObjetivo) => {
 
 // listar todos os objetivos financeiros
 export const listarObjetivosFinanceiros = async () => {
-
-};
+    try {
+        const objetivos = await db.objetivoFinanceiro.findMany();
+        return objetivos;  // Retorna os objetivos
+    } catch (erro) {
+        throw new Error('Erro ao listar objetivos');  // Lança o erro para ser tratado no controlador
+    }
+}
 
 // atualizar um objetivo financeiro
 export const atualizarObjetivoFinanceiro = async (id, atualizacoes) => {
@@ -19,5 +24,4 @@ export const atualizarObjetivoFinanceiro = async (id, atualizacoes) => {
 
 // deletar um objetivo financeiro
 export const deletarObjetivoFinanceiro = async (id) => {
-
 };
