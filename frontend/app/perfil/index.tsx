8ca3a57810ac9a52962ea5d6648fcdcef34be51c
João Paulo <jhonbrandaoks83@gmail.com>
2024-11-08 17:58:03 -0300

    3 tasks
    Implementar a regra de negócio de criar transações (Model e controller)
    
    Colocar a mascara no campo CPF da tela de perfil
    
    Na tela de nova conta o campo saldo inicial deve ser formatado do tipo Currency (R$ 1,500,00)

--- frontend/app/perfil/index.tsx
+++ frontend/app/perfil/index.tsx
@@ -1,13 +1,31 @@
-import { KeyboardAvoidingView, TextInput, TouchableOpacity, Text, Image, View} from "react-native";
+import React, { useState } from "react";
+import { Image, KeyboardAvoidingView, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { styles } from "../../assets/styles/perfil";
-import React from "react";
 
 export default function App() {
-    // texto padrão improtado como constante
     const texto = styles.text;
 
+    const [cpf, setCpf] = useState('');
+
+
+    const handleCpfChange = (text:string) => {
+        const onlyNumbers = text.replace(/\D/g, ''); 
+        let formattedCpf = onlyNumbers;
+
+        if (onlyNumbers.length > 3) {
+            formattedCpf = `${onlyNumbers.slice(0, 3)}.${onlyNumbers.slice(3)}`;
+        }
+        if (onlyNumbers.length > 6) {
+            formattedCpf = `${onlyNumbers.slice(0, 3)}.${onlyNumbers.slice(3, 6)}.${onlyNumbers.slice(6)}`;
+        }
+        if (onlyNumbers.length > 9) {
+            formattedCpf = `${onlyNumbers.slice(0, 3)}.${onlyNumbers.slice(3, 6)}.${onlyNumbers.slice(6, 9)}-${onlyNumbers.slice(9, 11)}`;
+        }
+
+        setCpf(formattedCpf); 
+    };
+
     return (
-        
         // container
         <KeyboardAvoidingView style={styles.container}>
             {/* botão de voltar */}
@@ -18,59 +36,59 @@ export default function App() {
 
                 {/* perfil */}
                 <TouchableOpacity style={styles.buttonIcon}>
-                <Image source={require('../../assets/icons/perfil.png')} style={styles.image}/> 
+                    <Image source={require('../../assets/icons/perfil.png')} style={styles.image}/> 
 
-                {/* botão de editar */}
-                <TouchableOpacity style={styles.editButton}>
-                <Image source={require('../../assets/icons/edit.png')} style={styles.editImage}/>
-                </TouchableOpacity>    
+                    {/* botão de editar */}
+                    <TouchableOpacity style={styles.editButton}>
+                        <Image source={require('../../assets/icons/edit.png')} style={styles.editImage}/>
+                    </TouchableOpacity>    
 
-                {/* texto do perfil */}
-                <Text style={{...texto, position: "absolute", alignSelf: "center", top: "110%"}}>Perfil</Text>
+                    {/* texto do perfil */}
+                    <Text style={{...texto, position: "absolute", alignSelf: "center", top: "110%"}}>Perfil</Text>
                 </TouchableOpacity>
 
+                {/* view que engloba os inputs */}
+                <View style={styles.viewInput}>
+                    {/* input do nome */}
+                    <Text style={{...texto, color: 'grey', alignSelf: 'flex-start', marginLeft: '10%'}}>Nome</Text>
+                    <TextInput 
+                        textContentType="username"
+                        autoCapitalize="none"
+                        autoComplete="username"
+                        autoCorrect={false}
+                        style={styles.input}
+                    />
 
-        {/* view que engloba os inputs */}
-            <View style={styles.viewInput}>
-            {/* input do nome */}
-            <Text style={{...texto, color: 'grey', alignSelf: 'flex-start', marginLeft: '10%'}}>nome</Text>
-            <TextInput 
-                textContentType="username"
-                autoCapitalize="none"
-                autoComplete="username"
-                autoCorrect={false}
-                style={styles.input}
-            />
+                    {/* input do email */}
+                    <Text style={{...texto, color: 'grey', alignSelf: 'flex-start', marginLeft: '10%'}}>Email</Text>
+                    <TextInput 
+                        textContentType="emailAddress"
+                        autoCapitalize="none"
+                        autoComplete="email"
+                        autoCorrect={false}
+                        style={styles.input}
+                    />
 
-            {/* input do email */}
-            <Text style={{...texto, color: 'grey', alignSelf: 'flex-start', marginLeft: '10%'}}>Email</Text>
-            <TextInput 
-                textContentType="emailAddress"
-                autoCapitalize="none"
-                autoComplete="email"
-                autoCorrect={false}
-                style={styles.input}
-            />
+                    {/* input do CPF */}
+                    <Text style={{...texto, color: 'grey', alignSelf: 'flex-start', marginLeft: '10%'}}>CPF</Text>
+                    <TextInput 
+                        keyboardType="numeric"
+                        textContentType="username"
+                        autoComplete="username"
+                        autoCorrect={false}
+                        style={styles.input}
+                        value={cpf}
+                        onChangeText={handleCpfChange}
+                        maxLength={14} // (formato 000.000.000-00)
+                    />
+                </View>
 
-            {/* input do cpf */}
-            <Text style={{...texto, color: 'grey', alignSelf: 'flex-start', marginLeft: '10%'}}>CPF</Text>
-            <TextInput 
-                keyboardType="numeric"
-                textContentType="username"
-                autoComplete="username"
-                autoCorrect={false}
-                style={styles.input}
-            />
-        </View>
-
-
-            {/* botão de slavar */}
-            <TouchableOpacity style={styles.button}> 
-                <Text style={{...texto, color: '#fff'}}>Salvar</Text>
-            </TouchableOpacity>
+                {/* botão de salvar */}
+                <TouchableOpacity style={styles.button}> 
+                    <Text style={{...texto, color: '#fff'}}>Salvar</Text>
+                </TouchableOpacity>
 
             </View>
-
         </KeyboardAvoidingView>
     );
 }
