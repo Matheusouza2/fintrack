554675c5b9cf4bd748c6b01b65e93ad2d3c7207e
Gabriel Garcez <ggarcez613@gmail.com>
2024-11-22 09:02:09 -0300

    atuliza transacao

--- backend/controllers/transacoesController.js
+++ backend/controllers/transacoesController.js
@@ -1,5 +1,7 @@
 //controller de transacoes do usuario cadastrar, alterar, listar, excluir e mostrar(com base nos id)
 
+import { Alterartransacoes } from "../models/transacoes"
+
 /**
  * lista todas as transacoes de um usuario.
  */
@@ -21,7 +23,18 @@ export const cadastrarTransacaoPorUsuario = (req, res) => {}
 /**
  * faz alteracoes nas transacoes de um usuario com base no id
  */
-export const alterarTransacaoPorUsuario = (req, res) => {}
+export const alterarTransacaoPorUsuario = async (req, res) => {
+  const { id } = req.params
+  const data = req.body
+
+  if (!id) return res.status(404).json({ message: 'id not found' });
+
+  const updatedTransaction = await Alterartransacoes(id, data)
+
+  if (!updatedTransaction) return res.status(404).json({ message: 'Erro ao atualizar transação'})
+
+  return res.status(200).json(updatedTransaction)
+}
 
 
 /*
