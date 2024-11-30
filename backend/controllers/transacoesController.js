486239ffbf139547b5769393840ebfbee036ba3d
luanny-g <luanny.ferreira28@gmail.com>
2024-11-30 00:36:08 -0300

    Implementar regra de negocio para excluir transacao, model e controller

--- backend/controllers/transacoesController.js
+++ backend/controllers/transacoesController.js
@@ -1,5 +1,7 @@
 //controller de transacoes do usuario cadastrar, alterar, listar, excluir e mostrar(com base nos id)
 
+import { Excluirtransacoes } from "../models/transacoes";
+
 /**
  * lista todas as transacoes de um usuario.
  */
@@ -27,7 +29,30 @@ export const alterarTransacaoPorUsuario = (req, res) => {}
 /*
  * exclui uma transacao de um usuario com base no id
  */
-export const excluirTransacaoPorUsuario = (req, res) => {}
+export const excluirTransacaoPorUsuario = async (req, res) => {
+    const{id: tranferenciaId} = req.params;
+    const userId = req.user.id;
+
+    try{
+        const transacao = await database.transacao.findUnique({
+            where: {id: tranferenciaId},
+        });
+
+        if(!transacao){
+            return res.status(403).json({error: "Essa transção não existe"});
+        }
+
+        if(transacao.userId !== userId){
+         return res.status(403).json({error: "Você não apagar essa!!!"})
+        }
+
+        await Excluirtransacoes(tranferenciaId);
+
+        res.status(200).json({message: "Transação exxcluída!!!"});
+    } catch(error){
+        res.status(500).json({error: "Deu erro, não foi apagada!!!"});
+    }
+};
 
 // module.exports = {
 //     listarTransacoesPorUsuario,
--- backend/models/transacoes.js
+++ backend/models/transacoes.js
@@ -8,6 +8,18 @@ export const Listartransacoes = (userId) => {}
 
 export const Alterartransacoes = (tranferenciaId, alteracoes) => {}
 
-export const Excluirtransacoes = (tranferenciaId) => {}
+export const Excluirtransacoes = async (tranferenciaId) => {
+     const transacao = await database.transacao.findUnique({
+        where: {id, tranferenciaId}, //olha se a função existe
+     });
+
+     if(!transacao){//Ver se tipo o usuário pode apagar essa transação
+        throw new Error("Você não apagar essa transação!!!");
+     }
+
+     await database.transacao.delete({//apaga a transferência
+        where: {id: transacoesPorId},
+     });
+};
 
 export const transacoesPorId = (tranferenciaId) => {}
